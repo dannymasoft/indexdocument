91192d7c211d4ed6c4e0f0e1a22c04b1e61e0573
dannymasoft <danny@masoft.net>
2017-09-05 08:49:09 -0500

    menu

---
+++ view/BienvenidaLateralView.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Bienvenido - aDocument 2015</title>
+    <?php include("view/modulos/cabecera.php"); ?>
+    </head>
+    <body  style="background-color: #F6FADE">
+    
+       <div id="wrapper">
+        <div class="overlay"></div>
+        
+      <?php include("view/modulos/menuLateral.php"); ?>
+       
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                       
+                   </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+ 		
+    </body>
+</html>
